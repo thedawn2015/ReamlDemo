98b761857b2f4ca85963b2233bc71daa701c3d2b
xiaowei <SimonX15@163.com>
2017-09-19 16:29:32 +0800

    增删改查

--- app/src/main/java/com/app/simon/realmdemo/MainActivity.kt
+++ app/src/main/java/com/app/simon/realmdemo/MainActivity.kt
@@ -2,10 +2,12 @@ package com.app.simon.realmdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,20 +19,52 @@ class MainActivity : AppCompatActivity() {
 
     private fun assignViews() {
         btn_add.setOnClickListener {
-            RealmHelper.getRealm().executeTransaction {
-                val user = it.createObject(User::class.java)
-                user.id = 101
-                user.name = "赵云"
-            }
+            val user1 = User()
+            user1.id = 101
+            user1.name = "赵云"
+
+            val user2 = User()
+            user2.id = 102
+            user2.name = "张飞"
+            RealmHelper.getRealm().executeTransactionAsync({
+                it.copyToRealmOrUpdate(user1)
+                it.copyToRealmOrUpdate(user2)
+            }, {
+                Log.i(TAG, "add success")
+            }, {
+                Log.i(TAG, "add error")
+            })
+            /*RealmHelper.getRealm().executeTransaction {
+                val user1 = it.createObject(User::class.java)
+                user1.id = 101
+                user1.name = "赵云"
+            }*/
         }
         btn_delete.setOnClickListener {
+            RealmHelper.getRealm().executeTransaction {
+                val results = it.where(User::class.java)
+                        .equalTo("name", "张飞")
+                        .findAll()
+                results.deleteAllFromRealm()
 
+            }
         }
         btn_update.setOnClickListener {
-
+            RealmHelper.getRealm().executeTransaction {
+                val user = it.where(User::class.java)
+                        .equalTo("name", "张飞")
+                        .findFirst()
+                user.name = "李四"
+            }
         }
         btn_query.setOnClickListener {
-
+            RealmHelper.getRealm()
+                    .where(User::class.java)
+//                    .equalTo("name", "赵云")
+                    .findAllAsync()
+                    .addChangeListener {
+                        Log.i(TAG, "query it=" + it.toString())
+                    }
         }
     }
 
@@ -41,7 +75,7 @@ class MainActivity : AppCompatActivity() {
                 .name("my_realm.realm") //文件名
                 .schemaVersion(0) //版本号
                 .build()*/
-        RealmHelper.getRealm()
+//        RealmHelper.getRealm()
     }
 
     override fun onDestroy() {
